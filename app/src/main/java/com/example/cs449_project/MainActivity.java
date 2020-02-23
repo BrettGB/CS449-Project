63f45bfd756fe8226504a0252888363eeef56437
Brett B <bbarnow17@gmail.com>
2020-02-23 13:15:01 -0600

    Sprint #1

--- app/src/main/java/com/example/cs449_project/MainActivity.java
+++ app/src/main/java/com/example/cs449_project/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.cs449_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Scale Selector Spinner
+        spinner = findViewById(R.id.scaleSelection);
+
+        //these next couple lines initialize the spinner with the data in
+        //R.array.scales_array, each option in the spinner is a string/charSequence from that array
+        //the spinner then gets set with the setAdapter method
+        ArrayAdapter<CharSequence> adapter;
+        adapter = ArrayAdapter.createFromResource(this, R.array.scales_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        //when selecting an item in the spinner
+        //if the first item is selected (...) then no message will appear
+        //but if any other item is selected, the message: "Selected: " (your item) will appear
+        //the first entry in the spinner is the default/unpickable option
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0){
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
+
 }

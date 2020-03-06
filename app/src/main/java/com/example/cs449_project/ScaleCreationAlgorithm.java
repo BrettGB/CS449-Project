1df5db3f37ea6308bcbbb2e36ffec6d6654502c4
Brett B <bbarnow17@gmail.com>
2020-03-05 18:03:13 -0600

    Sprint #2

---
+++ app/src/main/java/com/example/cs449_project/ScaleCreationAlgorithm.java
@@ -0,0 +1,12 @@
+package com.example.cs449_project;
+
+public class ScaleCreationAlgorithm {
+
+    String[] musicNotes = {"B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
+            "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"};
+
+    public String[] scaleCreator(String scale, String key){
+
+        return new String[] {" "};
+    }
+}
